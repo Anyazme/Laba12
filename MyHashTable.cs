7b1c81ff42b31cc5ee2db0be2104d70d373a0bbe
Anyazme <144984560+Anyazme@users.noreply.github.com>
2024-05-20 13:53:44 +0500

    Add files via upload

---
+++ MyHashTable.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bib10;
+
+namespace Laba12_2
+{	
+	public class MyHashTable<T> where T : IInit, ICloneable, new()
+	{
+		T[] table; //таблица 
+		int count = 0; // кол-во записей
+		double fillRatio; // к-т заполняемости таблицы 
+
+		public int Capacity => table.Length; //емкость, кол-во выделенной памяти 
+		public int Count => count; //текущее кол-во элементов 
+
+		//конструктор
+		public MyHashTable(int size = 10, double fillRatio = 0.72)
+		{
+			table = new T[size];
+			this.fillRatio = fillRatio;//заполняем коэффицент 
+
+		}
+
+		public void Print()
+		{
+			int i = 0;
+			foreach (T item in table)
+			{
+				Console.WriteLine($"{i} : {item}");
+				i++;
+			}
+		}
+
+
+		//private
+		int GetIndex(T data)
+		{
+			return Math.Abs(data.GetHashCode() % Capacity);
+		}
+
+		//добавление элемента в таблицу 
+		void AddData(T data)
+		{
+			if (data == null) return; // доб-ся пустой элемент 
+									  //ищем место
+			int index = GetIndex(data);
+			int current = index; //запомнили индекс 
+								 //уже занято 
+			if (table[index] != null)
+			{
+				//идем до конца таблицы или до первого пустого места 
+				while (current < table.Length && table[current] != null)
+				{
+					current++;
+				}
+
+				//если таблица закончилась 
+				if (current == table.Length)
+				{
+					//идем от начала таблицы до индекса, который запомнили 
+					current = 0;
+					while (current < index && table[current] != null)
+					{
+						current++;
+					}
+
+					//места нет
+					if (current == index)
+					{
+						throw new Exception("Нет места в таблице");
+					}
+				}
+			}
+
+			//Место найдено
+			table[current] = data;
+			count++;
+		}
+
+
+		public int FindItem(T data)
+		{
+			//находим индекс
+			int index = GetIndex(data);
+			//нет элемента
+			if (table[index] == null) return -1;
+
+			//есть элемент, совпадает
+			if (table[index].Equals(data))
+			{
+				return index;
+			}
+			else //не совпадает 
+			{
+				int current = index; // идем вниз по таблице 
+				while (current < table.Length)
+				{
+					if (table[current] != null && table[current].Equals(data))
+					{
+						return current;
+					}
+					current++;
+				}
+				current = 0; //идем с начала таблицы
+				while (current < index)
+				{
+					if (table[current] != null && table[current].Equals(data))
+					{
+						return current;
+					}
+					current++;
+				}
+				return -1; //не нашли
+
+			}
+
+		}
+
+		//поиск
+		public bool Contains(T data) //возвращает true или false
+		{
+			return !(FindItem(data) < 0);// значит ничего не нашли 
+		}
+
+		//удаление 
+		public bool RemoveData(T data) //лучше тем, что после удаленного элемента у элементов идет перехеширование
+		{
+			int index = FindItem(data);
+			if (index < 0) return false;
+
+			// Удаляем элемент из таблицы
+			table[index] = default;
+			count--;
+
+			// новое хеширование для элементов, следующих за удаленным элементом
+			int currentIndex = (index + 1) % Capacity;
+			while (table[currentIndex] != null)
+			{
+				T temp = table[currentIndex];
+				table[currentIndex] = default;
+				count--;
+				AddData(temp); // переставляем элемент на новое место
+				currentIndex = (currentIndex + 1) % Capacity;
+			}
+
+			return true;
+		}
+
+
+		//добавление 
+		public void AddItem(T item)
+		{
+			if (Count / Capacity > fillRatio)
+			{
+				T[] temp = (T[])table.Clone();//увеличиваем таблицу в 2 раза и переписываем всю информацию
+				table = new T[temp.Length * 2];
+				count = 0;
+				for (int i = 0; i < temp.Length; i++)
+					AddData(temp[i]);
+			}
+
+			//Добавляем новый элемент 
+			AddData(item);
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,114 +1,91 @@
-﻿using Bib10;
-namespace Laba12_1
+﻿using Laba12_2;
+using Bib10;
+using System;
+
+public class Program
 {
-	public class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		MyHashTable<Aircraft> myHash = new MyHashTable<Aircraft>();
+
+		bool ok = false;
+
+		while (!ok)
 		{
-			MyList<Aircraft> list = new MyList<Aircraft>();	
-			bool ok = false;
-			while (!ok)
+			Console.WriteLine("Меню:");
+			Console.WriteLine("1. Добавить воздушное судно");
+			Console.WriteLine("2. Вывести хеш таблицу");
+			Console.WriteLine("3. Проверить наличие воздушного судна");
+			Console.WriteLine("4. Удалить воздушное судно");
+			Console.WriteLine("5. Выход");
+
+			Console.Write("Выберите действие: ");
+			int choice = int.Parse(Console.ReadLine());
+
+			switch (choice)
 			{
-				try
-				{
-					Console.WriteLine("Меню:");
-					Console.WriteLine("1. Создать новый список заданного размера");
-					Console.WriteLine("2. Вывести список");
-					Console.WriteLine("3. Добавить воздушное судно в начало списка");
-					Console.WriteLine("4. Добавить воздушное судно в конец списка");
-					Console.WriteLine("5. Удалить воздушное судно из списка");
-					Console.WriteLine("6. Добавить в список элемент с заданным номером");
-					Console.WriteLine("7. Удалить все элементы с нечетными номерами");
-					Console.WriteLine("8. Выход");
-					Console.WriteLine("Введите ваш выбор:");
-
-					int answer = int.Parse(Console.ReadLine());
-
-					switch (answer)
+				case 1:
 					{
-						case 1:
-							{
-								Console.WriteLine("Размер списка?");
-								int size = int.Parse(Console.ReadLine());
-								list = new MyList<Aircraft>(size);
-								Console.WriteLine("Список создан");
-								break;
-							}
-
-						case 2:
-							{
-								list.PrintList();
-								break;
-							}
-
-						case 3:
-							{
-								Aircraft aircraft2 = new Aircraft();
-								aircraft2.RandomInit();
-								list.AddToBegin(aircraft2);
-								Console.WriteLine("Элемент добавлен в начало");
-								break;
-							}
-
-						case 4:
-							{
-								Aircraft aircraft3 = new Aircraft();
-								aircraft3.RandomInit();
-								list.AddToEnd(aircraft3);
-								Console.WriteLine("Элемент добавлен в конец");
-								break;
-							}
-
-						case 5:
-							{
-								Aircraft aircraft4 = new Aircraft();
-								aircraft4.Init();
-								if (!list.RemoveItem(aircraft4))
-								{
-									Console.WriteLine("Элемент не найден");
-								}
-								else
-								{
-									Console.WriteLine("Элемент удален");
-								}
-								break;
-							}
-
-						case 6:
-							{
-								Console.WriteLine("Введите индекс, куда добавить элемент:");
-								int index = int.Parse(Console.ReadLine());
-
-								Aircraft aircraft4 = new Aircraft();
-								aircraft4.RandomInit();
-
-								list.AddAtIndex(aircraft4, index);
-								Console.WriteLine("Элемент добавлен по индексу " + index);
-								break;
-							}
-
-						case 7:
-							{
-								list.RemoveAllIndexedElements();
-								Console.WriteLine("Удалены все элементы с нечетными индексами");
-								break;
-							}
-
-						case 8:
-							{
-								ok = true;
-								break;
-
-							}
+						Aircraft aircraft = new Aircraft();
+						aircraft.RandomInit();
+						myHash.AddItem(aircraft);
+						Console.WriteLine("Воздушное судно добавлено в хеш таблицу.");
 					}
-				}
+					break;
 
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Ошибка: {ex}");
-				}
+				case 2:
+					{
+						Console.WriteLine("Хеш таблица:");
+						myHash.Print();
+					}
+					break;
+
+				case 3:
+					{
+						Console.Write("Введите данные о воздушном судне для проверки наличия: ");
+						Aircraft aircraft1 = new Aircraft();
+						aircraft1.Init();
+						if (myHash.Contains(aircraft1))
+						{
+							Console.WriteLine("Воздушное судно присутствует в хеш таблице.");
+						}
+						else
+						{
+							Console.WriteLine("Воздушное судно отсутствует в хеш таблице.");
+						}
+					}
+					break;
+
+				case 4:
+					{
+						Console.Write("Введите данные о воздушном судне для удаления: ");
+						Aircraft aircraft2 = new Aircraft();
+						aircraft2.Init();
+						if (myHash.RemoveData(aircraft2))
+						{
+							Console.WriteLine("Воздушное судно удалено из хеш таблицы.");
+						}
+						else
+						{
+							Console.WriteLine("Воздушное судно не найдено в хеш таблице.");
+						}
+					}
+					break;
+
+				case 5:
+					{
+						ok = true;
+					}
+					break;
+
+				default:
+					Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
+					break;
 			}
 		}
+
+		Console.WriteLine("Программа завершена");
 	}
-} 
-		
+}
+
+
